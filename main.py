8c9fadba9a11e44e544409aaa8edc76c9985d004
elivanHF <elivanrr@gmail.com>
2021-11-28 17:39:08 -0400

    teste git

---
+++ main.py
@@ -0,0 +1,5 @@
+#Antações requisitos para desenvolvimento da aplicação:
+# 1- Pois desde  1/1/2016, com a Nota Técnica 2015/002, foi incluída uma nova regra de validação para autorização
+# da NF-e, que verifica se a NCM informada existe na tabela publicada pelo Ministério do
+# Desenvolvimento, Indústria e Comércio Exterior (MDIC).
+print('tá tudo certo')
